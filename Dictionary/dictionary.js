a534f7457237ce0990f119c478fd89406d0aab29
andre-luis-araujo <andre.araujo1@acad.ifma.edu.br>
2026-01-07 16:59:06 -0300

    Refactor Dictionary class and add ValuePair class

--- Dictionary/dictionary.js
+++ Dictionary/dictionary.js
@@ -1,57 +1,86 @@
-function defaultToString(item) {
-    if (item === null) {
-        return 'NULL';
-    } else if (item === undefined) {
-        return 'UNDEFINED';
-    } else if (typeof item === 'string' || item instanceof String) {
-        return `${item}`;
-    }
-return item.toString();
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+  toString() {
+    return `[#${this.key}: ${this.value}]`;
+  }
 }
 
-//Classe que guarda a chave e valor
-class ValuePair{
-    constructor(key, value){
-        this.key = key
-        this.value = value
+class Dicionario {
+  constructor() {
+    this.table = {};
+  }
+
+  // Verifica se a chave existe
+  hasKey(key) {
+    return Object.prototype.hasOwnProperty.call(this.table, key);
+  }
+
+  // Adiciona ou atualiza um elemento
+  set(key, value) {
+    if (key != null && value != null) {
+      this.table[key] = new ValuePair(key, value);
+      return true;
     }
-    toString(){
-        return `[#${this.key} : ${this.value}]`
+    return false;
+  }
+
+  // Remove um elemento pela chave
+  remove(key) {
+    if (this.hasKey(key)) {
+      delete this.table[key];
+      return true;
     }
+    return false;
+  }
+
+  // Retorna um valor específico
+  get(key) {
+    const valuePair = this.table[key];
+    return valuePair == null ? undefined : valuePair.value;
+  }
+
+  // Limpa o dicionário
+  clear() {
+    this.table = {};
+  }
+
+  // Retorna a quantidade de itens
+  size() {
+    return Object.keys(this.table).length;
+  }
+
+  // Verifica se está vazio
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  // Retorna array com todas as chaves
+  keys() {
+    return this.keyValues().map(valuePair => valuePair.key);
+  }
+
+  // Retorna array com todos os valores
+  values() {
+    return this.keyValues().map(valuePair => valuePair.value);
+  }
+
+  // Retorna array com todos os pares [chave, valor]
+  keyValues() {
+    return Object.values(this.table);
+  }
+
+  // Itera pelos valores, permitindo interrupção se retornar false
+  forEach(callbackFn) {
+    const valuePairs = this.keyValues();
+    for (let i = 0; i < valuePairs.length; i++) {
+      const result = callbackFn(valuePairs[i].key, valuePairs[i].value);
+      if (result === false) {
+        break;
+      }
+    }
+  }
 }
 
-//Classe dicionário
-class Dictionary{
-    constructor(toStrFn = defaultToString){
-        this.toStrFn = toStrFn
-        this.table = {}
-    }
-    //Inserir valores com chave e seu respectivo valor
-    set(key,value){
-        if(key != null && value != null){
-            const tableKey = this.toStrFn(key)
-            this.table[tableKey] = new ValuePair(key,value)
-            return true
-        }
-        return false
-    }
-    remove(key){}
-
-  
-    hasKey(key){
-        return this.table[this.toStrFn(key)] != null
-    }
-    get(key){}
-    clear(){}
-    size(){}
-    isEmpty(){}
-    keys(){}
-    values(){}
-    keyValues(){}
-    forEach(callbackFn){}
-}
-
-const dict = new Dictionary
-dict.set(1,2)
-dict.set(2,2)
-console.log(dict)
