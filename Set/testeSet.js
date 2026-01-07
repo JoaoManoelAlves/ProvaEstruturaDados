0fa179e71cff1335c2f420a839e94b68ba163d2b
JoaoManoelAlves <162204418+JoaoManoelAlves@users.noreply.github.com>
2026-01-07 16:08:10 -0300

    Adição de testes para Sets

---
+++ Set/testeSet.js
@@ -0,0 +1,16 @@
+const Set = require('./set.js')
+
+const SetA = new Set
+SetA.add(1)
+SetA.add(2)
+SetA.add(3)
+
+const SetB = new Set()
+SetB.add(3)
+SetB.add(4)
+SetB.add(5)
+
+console.log(SetA.union(SetB))
+console.log(SetA.intersection(SetB))
+console.log(SetA.difference(SetB))
+console.log(SetA.isSubsetOf(SetB))
