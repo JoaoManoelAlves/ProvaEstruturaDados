ef0e02911ae00ca6a2da3f3712a98e8b9f427b96
Ester Martins <sterre.msb@gmail.com>
2026-01-09 16:36:02 -0300

    Add TesteHash.js for HashTable

---
+++ HashTable/TesteHash.js
@@ -0,0 +1,13 @@
+const TesteHash = require('./hashtable.js')
+  
+const hash = new HashTable();
+hash.put('Bruno', 'bruno@email.com');
+hash.put('João', 'joao@email.com');
+hash.put('Ester', 'ester@email.com');
+hash.put('Andre', 'andre@email.com');
+console.log(hash.hashCode('Bruno') + ' - Bruno');
+console.log(hash.hashCode('Joao') + ' - Joao');
+console.log(hash.hashCode('Ester') + ' - Ester');
+console.log(hash.hashCode('Andre') + ' - Andre');
+
+console.log(hash.get('Ester')); // Saida: ester@email.com
