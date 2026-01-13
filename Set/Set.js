f9d3de8b6b915d98fc510e6ce3d70a7b3fd9ff3a
Bruno V <vitalb@acad.ifma.edu.br>
2026-01-13 20:31:11 -0300

    atualizaçao do nome da classe pra evitar conflito

--- Set/Set.js
+++ Set/Set.js
@@ -1,85 +1,85 @@
 
 //Set são listas que apresentam pares de chave e valor, porém sem elementos repetidos
-module.exports = class Set{
-    constructor(){
-        this.lista = {}
-        this.count = 0
+module.exports = class ListaSet {
+    constructor() {
+        this.lista = {};
+        this.count = 0;
     }
     //Verifica se ja existe um par de chave e valor no set
-    has(element){
-        return Object.prototype.hasOwnProperty.call(this.lista, element)
+    has(element) {
+        return Object.prototype.hasOwnProperty.call(this.lista, element);
     }
     //Adicionar uma chave e valor
-    add(element){
-        if(!this.has(element)){
-            this.lista[element] = element
-            this.count++
-            return true
+    add(element) {
+        if (!this.has(element)) {
+            this.lista[element] = element;
+            this.count++;
+            return true;
         }
-        return false
+        return false;
     }
     //Deleta a chave e valor
-    delete(element){
-        if(this.has(element)){
-            delete this.lista[element]
-            this.count--
-            return true
+    delete(element) {
+        if (this.has(element)) {
+            delete this.lista[element];
+            this.count--;
+            return true;
         }
-        return false
+        return false;
     }
     //A junção de todos os dados que eles tem
-    union(anotherSet){
-        const unionSet = new Set()
+    union(anotherSet) {
+        const unionSet = new ListaSet();
         //Para cada valor do set, vai ser colocado no unionSet
-        this.values().forEach(value => unionSet.add(value)) 
-        anotherSet.values().forEach(value => unionSet.add(value)) //o método add ignora valores repetidos
-        return unionSet
+        this.values().forEach(value => unionSet.add(value));
+        anotherSet.values().forEach(value => unionSet.add(value)); //o método add ignora valores repetidos
+        return unionSet;
     }
     //O que eles tem em comum?
-    intersection(anotherSet){
-        const intersectionSet = new Set()
+    intersection(anotherSet) {
+        const intersectionSet = new ListaSet();
         this.values().forEach(value => {
-            if(anotherSet.has(value)){
-                intersectionSet.add(value)
+            if (anotherSet.has(value)) {
+                intersectionSet.add(value);
             }
-        })
-        return intersectionSet
+        });
+        return intersectionSet;
     }
     //O que um tem que o outro não tem?
-    difference(anotherSet){
-        const differenceSet = new Set()
+    difference(anotherSet) {
+        const differenceSet = new ListaSet();
         this.values().forEach(value => {
-            if(!anotherSet.has(value)){
-                differenceSet.add(value)
+            if (!anotherSet.has(value)) {
+                differenceSet.add(value);
             }
-        })
-        return differenceSet
+        });
+        return differenceSet;
     }
     //O conjunto existe dentro do outro conjunto?
-    isSubsetOf(anotherSet){
-        if(this.size() > anotherSet.size()) return false;
-        let isSubset = true
+    isSubsetOf(anotherSet) {
+        if (this.size() > anotherSet.size()) return false;
+        let isSubset = true;
         this.values().every(value => {
-            if(!anotherSet.has(value)){
-                isSubset = false
-                return false
+            if (!anotherSet.has(value)) {
+                isSubset = false;
+                return false;
             }
-            return true
-        })
-        return isSubset
+            return true;
+        });
+        return isSubset;
     }
-    size(){
-        return this.count
+    size() {
+        return this.count;
     }
-    isEmpty(){
-        return this.count == 0
+    isEmpty() {
+        return this.count == 0;
     }
-    clear(){
-        this.lista = {}
-        this.count = 0
+    clear() {
+        this.lista = {};
+        this.count = 0;
     }
     //Valores dos sets
-    values(){
-        return Object.values(this.lista)
+    values() {
+        return Object.values(this.lista);
     }
-}
+};
--- Set/testeSet.js
+++ Set/testeSet.js
@@ -1,16 +1,16 @@
-const Set = require('./Set.js');
+const ListaSet = require('./Set.js');
 
-const SetA = new Set
-SetA.add(1)
-SetA.add(2)
-SetA.add(3)
+const SetA = new ListaSet();
+SetA.add(1);
+SetA.add(2);
+SetA.add(3);
 
-const SetB = new Set()
-SetB.add(3)
-SetB.add(4)
-SetB.add(5)
+const SetB = new ListaSet();
+SetB.add(3);
+SetB.add(4);
+SetB.add(5);
 
-console.log(SetA.union(SetB))
-console.log(SetA.intersection(SetB))
-console.log(SetA.difference(SetB))
-console.log(SetA.isSubsetOf(SetB))
+console.log(SetA.union(SetB));
+console.log(SetA.intersection(SetB));
+console.log(SetA.difference(SetB));
+console.log(SetA.isSubsetOf(SetB));
