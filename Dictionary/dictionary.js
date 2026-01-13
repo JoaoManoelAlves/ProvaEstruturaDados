6d5faf8c7983c93dded97fe74987df18d7b474a4
Bruno V <vitalb@acad.ifma.edu.br>
2026-01-13 20:00:15 -0300

    adiçao do arquivo de testes

--- Dictionary/dictionary.js
+++ Dictionary/dictionary.js
@@ -99,10 +99,3 @@ class Dictionary{
 }
 
 }
-
-const dict = new Dictionary
-dict.set(1,2)
-dict.set(9,2)
-dict.set(3,19)
-dict.set(4, 20)
-
---
+++ Dictionary/testeDict.js
@@ -0,0 +1,8 @@
+const Dictionary = require('./dictionary')
+
+const dict = new Dictionary()
+dict.set(1,2)
+dict.set(9,2)
+dict.set(3,19)
+dict.set(4, 20)
+console.log(dict)
