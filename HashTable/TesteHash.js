7c0fac2c92c377fd51cb7c3b70478f9ee698f566
Ester Martins <sterre.msb@gmail.com>
2026-01-13 16:18:54 -0300

    Update TesteHash.js

--- HashTable/TesteHash.js
+++ HashTable/TesteHash.js
@@ -1,6 +1,6 @@
 const TesteHash = require('./hashtable.js')
   
-const hash = new HashTable();
+const hash = new TesteHash();
 hash.put('Bruno', 'bruno@email.com');
 hash.put('João', 'joao@email.com');
 hash.put('Ester', 'ester@email.com');
